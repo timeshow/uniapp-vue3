1e14ebc61ae583931d6c4b24d554f72de77e17de
timeshow <wdiyer@163.com>
2025-03-01 20:27:38 +0800

    feat: subPackages

---
+++ pages/user/info/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <view>info</view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/user/set/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <view>set</view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
